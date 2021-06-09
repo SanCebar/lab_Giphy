25fa800ebf6a281c981ee24294bb95b28ab1c7dc
SanCebar <r19velasco@gmail.com>
2021-06-09 13:51:13 -0700

    Need to update seach box and clear screen

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ const limit = 9;
 const rating = "g";
 const lang = "en";
 
+const display = document.querySelector(".gif-results");
 const gif_form = document.querySelector("form");
 gif_form.addEventListener("submit", getResults);
 
 async function getResults(evt) {
-    console.log("form submitted!");
     evt.preventDefault();
-    
+    console.log("form submitted!");
     const searchTerm = evt.target.giffy.value;
     const apiUrl = `http://api.giphy.com/v1/gifs/search?q=${searchTerm}&limit=${limit}&rating=${rating}&lang=${lang}&api_key=${api_key}`;
 
@@ -21,13 +21,21 @@ async function getResults(evt) {
     const responseData = await response.json();
     const data = responseData.data;
     console.log(data);
-    //display
+
     displayResults(data);
 }
 
 function displayResults (data) {
-    const display = document.querySelector(".gif-results");
     data.forEach(element => {
-        display.innerHTML += `<img src="${element.image.original.url}">`;
+        display.innerHTML += `<img alt="${element.title}" src="${element.images.original.url}" >`;
     });
 }
+
+// function handleFormSubmit (evt) {
+//     evt.preventDefault();
+//     console.log("form submitted!");
+//     const searchTerm = evt.target.giffy.value;
+    
+//     const data = getResults(searchTerm);
+//     displayResults(data);
+// }
